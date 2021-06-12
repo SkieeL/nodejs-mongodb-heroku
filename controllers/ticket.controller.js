2d46ba8ba599b5179cb9bd86e27feb32e530500a
Ezequiel Mahafud <ezemahafud@gmail.com>
2021-06-11 22:16:50 -0300

    Fix

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -1,10 +1,10 @@
 const db = require('../models/db');
 
-// Retrieve all users from the database.
+// Retrieve all tickets from the database.
 exports.findAll = (req, res) => {
     var condition = {};
 
-    db.getInstance().collection('users').find(condition).toArray().then(data => {
+    db.getInstance().collection('tickets').find(condition).toArray().then(data => {
         res.send(data);
     })
         .catch(err => {
